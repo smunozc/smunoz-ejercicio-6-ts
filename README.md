61f1c9bbfbefc6a92e65322797dde98b0fee4283
smunozc <salvador.munoz0109@gmail.com>
2020-11-12 09:53:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smunoz-ejercicio-6-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/smunoz-ejercicio-6-ts)
---
+++ index.ts
@@ -0,0 +1,16 @@
+// En este ejercicio debéis crear una clase "Person" con las propiedades privadas "name" y "age" y la propiedad pública "saySomething". Añadid también el método "myNameIs" para que saque por consola el valor de la propidad "name".
+//En su constructor, debéis dar un valor a cada propiedad (sin contar "myNameIs") teniendo claro que sus tipos son cadena de texto, número y función que recibe cadena y no devuelve nada, respectivamente.
+// Luego, cread una clase "Developer" que herede de "Person" y que tenga la propiedad pública "bestLanguage". El tipo de la propiedad "bestLanguage" es función que recibe cadena y no devuelve nada.
+// En el constructor de "Developer", debéis añadir los parámetros al constructor de la clase padre (super) y setear la propiedad "bestLanguage" sabiendo que es de tipo función que recibe una cadena.
+// Cuando acabéis, debéis descomentar las siguientes líneas de código y colocarlas despúes de todo lo que añadáis:
+
+// const developer = new Developer(
+//   "pedro",
+//   35,
+//   something => console.log(something),
+//   language => console.log(language + " is the best language")
+// );
+
+// developer.myNameIs();
+// developer.saySomething('algo');
+// developer.bestLanguage('Javascript');
